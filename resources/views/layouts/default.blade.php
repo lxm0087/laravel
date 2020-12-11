9ed8ad08be6f1f856bd566e10f7f9653236011ab
lxm <350834527@qq.com>
2020-12-11 11:42:42 +0800

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>@yield('title','Weibo App')- Laravel 新手入门教程</title>
+</head>
+<body>
+ @yield('content')
+</body>
+</html>
