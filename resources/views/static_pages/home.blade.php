30a7f8988d31bc76f84a04d6f821b08f82929646
lxm <350834527@qq.com>
2020-12-18 19:17:38 +0800

    commit messages

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -6,7 +6,7 @@
         <p class="lead">你现在看到的是示例页</p>
         <p>一切将从这里开始</p>
         <p>
-            <a href="#" class="btn btn-lg btn-success" role="button">现在注册</a>
+            <a href="{{route('signup')}}" class="btn btn-lg btn-success" role="button">现在注册</a>
         </p>
     </div>
 @stop
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','注册页面')
+@section('content')
+    <h1>注册</h1>
+    @stop
