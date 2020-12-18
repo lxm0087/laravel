7d36a988ed7e42f99352b74f97a65da9d87b8a1e
lxm <350834527@qq.com>
2020-12-18 16:45:23 +0800

    初始化样式

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title','web App')</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a href="navbar-brand" href="/">weiboApp</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+        </ul>
+    </div>
+</nav>
+<div class="container">
+    @yield('content')
+</div>
+</body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,12 @@
+ @extends('layouts.default')
+ @section('title','新入门教程')
+@section('content')
+    <div class="jumbotron">
+        <h1>hello laravel</h1>
+        <p class="lead">你现在看到的是示例页</p>
+        <p>一切将从这里开始</p>
+        <p>
+            <a href="#" class="btn btn-lg btn-success" role="button">现在注册</a>
+        </p>
+    </div>
+@stop
