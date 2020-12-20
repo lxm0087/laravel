dec26cd878a126a37b1dad26f22d928973fed377
lxm0087 <350834527@qq.com>
2020-12-20 11:17:32 +0800

    “laravrl/frame update to 8.0

--- public/js/app.js
+++ public/js/app.js
@@ -37339,8 +37339,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\phpstudy_pro\WWW\laravel\Laravel\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\phpstudy_pro\WWW\laravel\Laravel\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! E:\phpstudy\phpstudy_pro\WWW\laravel\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! E:\phpstudy\phpstudy_pro\WWW\laravel\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
