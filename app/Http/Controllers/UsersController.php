fa0a9f26ce58278bf80e0346d1ca9b08493ce048
lxm <350834527@qq.com>
2020-12-21 17:27:38 +0800

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,17 @@ use Auth;
 class UsersController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('auth',[
+            'except' => ['show','create','store']
+        ]);
+
+        $this->middleware('guest',[
+            'only' =>['create']
+        ]);
+    }
+
     public function create(){
         return view('users.create');
     }
@@ -37,10 +48,12 @@ class UsersController extends Controller
     }
 
     public function edit(User $user){
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
     public function update(User $user,Request $request){
+        $this->authorize('update',$user);
        $this->validate($request,[
             'name' => 'required|unique|min:3|max:50',
             'password'=>'nullable|confirmed|min:6'
--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,10 +18,12 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
+        $guards = empty($guards) ? [null] : $guards;
         if (Auth::guard($guard)->check()) {
+            session()->flash('info', '您已登录，无需再次操作。');
             return redirect(RouteServiceProvider::HOME);
         }
 
-        return $next($request);
+//        return $next($request);
     }
 }
