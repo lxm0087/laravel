1ece45f18029a48440968e5558a4ee42f16b920c
lxm <350834527@qq.com>
2020-12-25 17:41:44 +0800

    用户微博列表

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
         // $this->call(UserSeeder::class);
         Model::unguard();
 
-        $this->call(UsersTableSeeder::class);
-
+//        $this->call(UsersTableSeeder::class);
+        $this->call(StatusesTableSeeder::class);
         Model::reguard();
     }
 }
---
+++ database/seeds/StatusesTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\seeds;
+
+use Illuminate\Database\Seeder;
+
+class StatusesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
