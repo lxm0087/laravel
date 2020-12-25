36556222f95fc954e1370d1321b3aacd464071e6
lxm <350834527@qq.com>
2020-12-25 15:22:05 +0800

    移除邮件发送的from选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,7 +20,7 @@ class UsersController extends Controller
             'only' =>['create']
         ]);
 
-        $this->middleware('throttle:10,10',[
+        $this->middleware('throttle:10,60',[
             'only' => ['store']
         ]);
     }
@@ -84,12 +84,10 @@ class UsersController extends Controller
     public function sendEmailConfirmationTo($user){
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = "350834527@qq.com";
-        $name = 'lxm';
         $to = $user->email;
         $subject = '感谢注册 Weibo 应用！请确认你的邮箱。';
-        Mail::send($view,$data,function ($message) use ($from,$name,$to,$subject){
-            $message->from($from,$name)->to($to)->subject($subject);
+        Mail::send($view,$data,function ($message) use ( $to,$subject){
+            $message->to($to)->subject($subject);
         });
     }
 
